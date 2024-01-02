c7825a9fcf3a6992a8d04ea7aa4e165c5f20b0ec
MoonstruckOccultist <justgive1234@gmail.com>
2024-01-02 13:09:21 -0700

    started working on tense changer, just need to figure out how to apply future, (also added 11am since i forgot)

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var tBlock = $('#timeblock');
+var tBlock = document.querySelectorAll('.time-block');
 var cDay = $('#currentDay');
 var saveConf = $('#saveConf');
 var saveBtn = $('.saveBtn');
 
 
+// var currentHr = dayjs().format('h');
+// console.log(currentHr);
+// var soloTB = tBlock[0];
+// var STBtense = soloTB.classList[2];
+// console.log(STBtense);
+// if (STBtense != "future") {
+//   STBtense = "future";
+//   console.log(STBtense);
+//   console.log(soloTB);
 
-console.log(saveBtn.parent().attr('id'))
+// }
+
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -29,31 +39,86 @@ $(function () {
     var confClock = 1;
     if (task.length > 0) {
       localStorage.setItem(`${hrNum}`, JSON.stringify(task))
+      saveConf.text("task succesfully saved");
       confClock = 1;
       var showConf = setInterval(function () {
         if (confClock > 0) {
-          saveConf.text("task succesfully saved");
           confClock--;
         } else {
           saveConf.text("");
-          clearInterval(confClock);
+          clearInterval(showConf);
         }
       }, 1000);
     }
   })
-
-
-
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  var currentHr = dayjs().format('h');
+  // var ampmCheck = dayjs().format('H');
+  var ampmCheck = 11;
+  if (ampmCheck > 8 && ampmCheck < 18) {
+    // find present and set past present or future
+    // for tblock[i].id
+    var thePresent = "";
+    for (var i = 0; i < 9; i++) {
+      var hrIdCheck = tBlock[i].id.split("-")[1];
+      console.log(hrIdCheck);
+      if (hrIdCheck == currentHr) {
+        thePresent = tBlock[i];
+      }
+    }
+    console.log(thePresent);
+
+    var xPast = ampmCheck - 9;
+    for (var n = 0; xPast > n; n++) {
+      tBlock[n].classList.remove("present");
+      tBlock[n].classList.remove("future");
+      tBlock[n].classList.add("past");
+    }
+    var xFuture = "";
+    // Figure out how to call from the end of an array and how to set up xFuture
+
+    thePresent.classList.remove("past");
+    thePresent.classList.remove("future");
+    thePresent.classList.add("present");
+
+  } else if (ampmCheck < 9) {
+    for (var a = 0; 9 > a; a++) {
+      var soloTB = tBlock[a];
+      var STBtense = soloTB.classList[2];
+      if (STBtense == "present") {
+        soloTB.classList.remove("present");
+        soloTB.classList.add("future")
+      }
+      if (STBtense == "past") {
+        soloTB.classList.remove("past");
+        soloTB.classList.add("future");
+      }
+    }
+  } else {
+    for (var p = 0; 9 > p; p++) {
+      var soloTB = tBlock[p];
+      var STBtense = soloTB.classList[2];
+      if (STBtense == "present") {
+        soloTB.classList.remove("present");
+        soloTB.classList.add("past")
+      }
+      if (STBtense == "future") {
+        soloTB.classList.remove("future");
+        soloTB.classList.add("past");
+      }
+    }
+  }
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-
+  var day = dayjs();
 });
