7490bafa1591fc95d5f99c97ab7d193da03d2373
MoonstruckOccultist <justgive1234@gmail.com>
2024-01-02 16:07:11 -0700

    added future tense class changer and date

--- script.js
+++ script.js
@@ -7,35 +7,19 @@ var saveConf = $('#saveConf');
 var saveBtn = $('.saveBtn');
 
 
-// var currentHr = dayjs().format('h');
-// console.log(currentHr);
-// var soloTB = tBlock[0];
-// var STBtense = soloTB.classList[2];
-// console.log(STBtense);
-// if (STBtense != "future") {
-//   STBtense = "future";
-//   console.log(STBtense);
-//   console.log(soloTB);
-
-// }
-
-
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // event listener checking save button clicks, will execute code to obtain the user's input and store thatl locally
   saveBtn.on('click', function (event) {
     var targ = event.target;
+    // if they click on the symbol it could cause problems if i didn't ensure targ = the button
     if (targ.localName == "i") {
       targ = targ.parentElement;
     }
     var hrNum = targ.parentElement.id;
-    console.log(hrNum);
+
     var task = $(`#${hrNum}`).children('textarea')[0].value;
 
+    // displays a message that the task was saved succesfully
     var confClock = 1;
     if (task.length > 0) {
       localStorage.setItem(`${hrNum}`, JSON.stringify(task))
@@ -51,41 +35,44 @@ $(function () {
       }, 1000);
     }
   })
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
   var currentHr = dayjs().format('h');
-  // var ampmCheck = dayjs().format('H');
-  var ampmCheck = 11;
+  var ampmCheck = dayjs().format('H');
+
+  // checks if it is currently within the hours shown between 9-5
   if (ampmCheck > 8 && ampmCheck < 18) {
     // find present and set past present or future
-    // for tblock[i].id
     var thePresent = "";
     for (var i = 0; i < 9; i++) {
       var hrIdCheck = tBlock[i].id.split("-")[1];
-      console.log(hrIdCheck);
       if (hrIdCheck == currentHr) {
         thePresent = tBlock[i];
       }
     }
-    console.log(thePresent);
 
+    // will color code anything determined as the past by changing the class
     var xPast = ampmCheck - 9;
     for (var n = 0; xPast > n; n++) {
       tBlock[n].classList.remove("present");
       tBlock[n].classList.remove("future");
       tBlock[n].classList.add("past");
     }
-    var xFuture = "";
-    // Figure out how to call from the end of an array and how to set up xFuture
-
+    // will color code anything determined as the future by changing the class
+    var xFuture = 17 - ampmCheck;
+    var h = 8
+    for (var i = 0; i < xFuture; i++) {
+      tBlock[h].classList.remove("present");
+      tBlock[h].classList.remove("past");
+      tBlock[h].classList.add("future");
+      h--;
+    }
+     // will color code what was determined as the present by changing the class
     thePresent.classList.remove("past");
     thePresent.classList.remove("future");
     thePresent.classList.add("present");
 
   } else if (ampmCheck < 9) {
+    // if app opened ealier than 9am, will color code everything as the future via class change
     for (var a = 0; 9 > a; a++) {
       var soloTB = tBlock[a];
       var STBtense = soloTB.classList[2];
@@ -99,6 +86,7 @@ $(function () {
       }
     }
   } else {
+    // if app opened later than 5pm, will color code everything as the future via class change
     for (var p = 0; 9 > p; p++) {
       var soloTB = tBlock[p];
       var STBtense = soloTB.classList[2];
@@ -113,12 +101,19 @@ $(function () {
     }
   }
 
-
-
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  for (var i = 0; i < 9; i++) {
+    var savedTask = JSON.parse(localStorage.getItem(tBlock[i].id));
+    console.log(savedTask)
+    if (savedTask != null) {
+      $(`#${tBlock[i].id}`).children('.description').text(savedTask);
+    }
+  }
+
+
   // TODO: Add code to display the current date in the header of the page.
-  var day = dayjs();
+  cDay.text(dayjs().format('dddd, MMMM DD'))
+
 });
